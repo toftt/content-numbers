afaa3771066bd42ef6cd6ea82cc75bccb1961603
toftt <jtoft01@gmail.com>
2016-07-07 15:19:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-$('input').bind("enterKey", function(e) {
+var last_value = '';
+var domain = 'se';
+
+$('#link').bind("enterKey", function(e) {
   $('#getNumbers').click();
 });
 $('input').keyup(function(e) {
@@ -7,11 +10,27 @@ $('input').keyup(function(e) {
   }
 });
 
+$('.button').click(function() {
+  $('.button').toggleClass('active');
+  if ($('#c_button').hasClass('active')) {
+    $('#copy_paste').show();
+    $('#search').hide();
+  } else {
+    $('#search').show();
+    $('#copy_paste').hide();
+  }
+});
+
 $('#getNumbers').click(function() {
+  getNumbers($('#link').val());
+});
+$('#search_field').keyup(searchContent);
+
+function getNumbers(link) {
   $('#middle').html('<img src="style/img/loader.svg">' + '<p class="movie_number">0%</p>');
   //let patt = /viaplay.se\/\w+\/[\w-]+/i;
-  let patt = /viaplay.[sfdn][eoki]\/\w+\/[\w-.]+/i;
-  let match = $('#link').val().match(patt);
+  let patt = /viaplay.[sfdn][eoki]\/\w+\/[\w-\.]+/i;
+  let match = link.match(patt);
 
   if (match) {
 
@@ -23,13 +42,13 @@ $('#getNumbers').click(function() {
       if (status != 'success') $('#middle').html('');
       // detect if movie or series
       if (data['_embedded']['viaplay:blocks']['0']['type'] == 'product') {
-        
+
         let guid = data['_embedded']['viaplay:blocks'][0]['_embedded']['viaplay:product']['system']['guid'];
 
         $('#middle').html("<p class='movie_number'>" + guid + '</p>');
         $('.movie_number').OneClickSelect();
       } else {
-        
+
         let season_num = data['_embedded']['viaplay:blocks'].length - 2;
         let season_arr = [];
         let avail_seasons = [];
@@ -49,7 +68,7 @@ $('#getNumbers').click(function() {
             let i = 1;
             for (let key in blocks) {
               episodes += '<span class="boxie">Episode ' + i + ':</span>';
-              content_numbers += '<span class="num-boxie">' + blocks[key].system.guid + '</span><br>';
+              content_numbers += '<span class="num-boxie">' + blocks[key].system.guid + '</span>';
               i++;
             }
             let title = '<div class="season-num">Season ' + avail_seasons[num] + '</div>';
@@ -57,7 +76,7 @@ $('#getNumbers').click(function() {
             content_numbers = '<div class="numbers">' + content_numbers + '</div>';
             season_arr[num] = '<div class="season">' + title + episodes + content_numbers + '</div>';
             count++;
-            $('#middle').html('<img src="style/img/loader.svg">' + '<p class="movie_number">' + Math.round((count/season_num) * 100) + '%</p>');
+            $('#middle').html('<img src="style/img/loader.svg">' + '<p class="movie_number">' + Math.round((count / season_num) * 100) + '%</p>');
             if (count == season_num) {
               $('#middle').html(season_arr.join(''));
               $('html, body').animate({
@@ -73,22 +92,58 @@ $('#getNumbers').click(function() {
       $('#middle').html("<p class='movie_number'>404 - page not found</p>");
     });
   } else $('#middle').html("<p class='movie_number'>Invalid link</p>");
-});
+}
 
-$.fn.OneClickSelect = function(){
-  return $(this).on('click',function(){
-    
-     var range, selection;
-     if (window.getSelection) {
-        selection = window.getSelection();        
-        range = document.createRange();
-        range.selectNodeContents(this);
-        selection.removeAllRanges();
-        selection.addRange(range);
+function searchContent() {
+  if ($('#search_field').val() === '') $('#results').html('');
+  if ($('#search_field').val() !== last_value && $('#search_field').val() !== '') {
+    $('#results').html('');
+    var URL = 'https://content.viaplay.' + domain + '/pcdash-' + domain + '/autocomplete?query=' + $('#search_field').val();
+    console.log(URL);
+    $.get(URL, function(data) {
+      console.log(data);
+      if ('_embedded' in data['_embedded']['viaplay:blocks'][0]) {
+        $('#results').html('');
+        var results = data['_embedded']['viaplay:blocks'][0]['_embedded']['viaplay:products'];
+        for (var result of results) {
+          var title = '';
+          if (result.type === 'movie') {
+            title = result.content.title;
+            var end = result.system.availability.end;
+          } else if (result.type === 'series') title = result.content.series.title;
+          var year = result.content.production.year;
+          var path = result._links.self.href;
+          path = path.replace('https://content.viaplay.' + domain + '/pcdash-' + domain, '');
+          path = 'http://viaplay.' + domain + path.replace('?partial=true&block=1', '');
+          console.log(path);
+          $('#results').append('<div class="link" link="' + path + '">' + title + ' (' + year + ')</div>');
+          if ($('#search_field').val() === '') $('#results').html('');
+        }
+        $('.link').click(function() {
+          $('#results').html('')
+          console.log(this);
+          getNumbers($(this).attr('link'));
+        });
+      }
+    });
+  }
+  last_value = $('#search_field').val();
+}
+
+$.fn.OneClickSelect = function() {
+  return $(this).on('click', function() {
+
+    var range, selection;
+    if (window.getSelection) {
+      selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(this);
+      selection.removeAllRanges();
+      selection.addRange(range);
     } else if (document.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(this);
-        range.select();
+      range = document.body.createTextRange();
+      range.moveToElementText(this);
+      range.select();
     }
   });
 };
